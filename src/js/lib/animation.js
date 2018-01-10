85a2733b2b5989321c0d6e6bdf691e7955685ae9
Lusjen <lusjenh@mail.ru>
2018-01-10 14:44:27 +0200

    change files

--- src/js/lib/animation.js
+++ src/js/lib/animation.js
@@ -7,4 +7,4 @@ tl
   .fromTo('.header__top', 1, {y:-100}, {y:0})
   .fromTo('.jobs', 1, {x:500}, {x:0}, 1)
   .staggerFromTo('.nav__item', 0.5, {y:-100}, {y:0}, 0.25)
-  .staggerFromTo('.main__col', 1, {x:2000,scale:0.1}, {x:0,scale:1}, 0.5);
+  .staggerFromTo('.main__col-wrapp', 1, {x:2000,scale:0.1}, {x:0,scale:1}, 0.5);
